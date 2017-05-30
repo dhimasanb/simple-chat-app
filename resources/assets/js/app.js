cedc3c7502c54550df64efde0e4b242954b0290a
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-05-31 04:33:18 +0700

    Add vue component

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -16,6 +16,9 @@ window.Vue = require('vue');
  */
 
 Vue.component('example', require('./components/Example.vue'));
+Vue.component('chat-message', require('./components/ChatMessage.vue'));
+Vue.component('chat-log', require('./components/ChatLog.vue'));
+Vue.component('chat-composer', require('./components/ChatComposer.vue'));
 
 const app = new Vue({
     el: '#app'
